6097c8ceef6a70c5fd08cec4f45341781d21a1ee
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-05-13 20:08:04 +0000

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modalMessage=document.getElementById("modal-message");
+let modal=document.getElementById("modal");
+let hearts=document.getElementsByClassName("like-glyph");
+for(const heart of hearts ){
+heart.addEventListener("click",(e)=>{
+  e.preventDefault();
+  mimicServerCall()
+  .then (function (response){
+    if (heart.textContent===EMPTY_HEART)
+    {
+      heart.textContent=FULL_HEART;
+      heart.classList.add("activated-heart");
+    }
+    else {
+      heart.textContent=EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    }
 
+  })
+  .catch ((error)=>{
+    modal.classList.remove("hidden")
+    modalMessage.innerHTML=error;
+    //throw ("Error",error);
+    setTimeout(function(){
+      modal.classList.add("hidden")
+    } ,3000);
+  });
 
+})
 
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
